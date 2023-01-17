7f1c6c3cab764ad5943f58c65a066af056160ae2
Yuhan Zhao <aidenzhao@hotmail.com>
2023-01-17 16:52:37 -0500

    Complete all functionality for Day_8

--- Day_8/Caesar_cipher.py
+++ Day_8/Caesar_cipher.py
@@ -9,15 +9,16 @@ def cipher(message, shift, option):
     encoded_message = ""
     for i in message.lower():
         
+        if i not in alphabet:
+            encoded_message += i
+            continue
+
         #shift = eval(option_dict[option] + str(shift))
         index_before = alphabet.index(i)
         # "5+10"
         index_after = eval(str(index_before) + option_dict[option] + str(shift))
-        if index_after > 25:
-            index_after -= 26
+        index_after %= 26
 
         encoded_message += alphabet[index_after]
     
     return encoded_message
-
-print(cipher("hello", 7, "encode"))
--- Day_8/main.py
+++ Day_8/main.py
@@ -2,8 +2,12 @@ from ascii_art import logo
 from Caesar_cipher import cipher
 
 print(logo)
-to_do = input("Enter 'encode' to encrypt a message, enter 'decode' to decrypt\n")
-message = input("Enter the message\n")
-shift = int(input("Enter shift number\n"))
 
-print("The message after {} is ".format(to_do) + cipher(message, shift, to_do))
+run = True
+while run:
+    to_do = input("Enter 'encode' to encrypt a message, enter 'decode' to decrypt\n")
+    message = input("Enter the message\n")
+    shift = int(input("Enter shift number\n"))
+
+    print("The message after {} is ".format(to_do) + cipher(message, shift, to_do))
+    run = {"yes": True, "no": False}[input("Would you like to go again? Type 'yes' or 'no': ")]
